dd8589fdab92a7779eb628f5d1c6096772abb48b
APIMASH <apimash@live.com>
2013-08-31 17:19:22 -0400

    Initial Load

---
+++ Windows8/APIMASH_INSTAGRAM_API/APIMASH_INSTAGRAM_API/js/apiglobals.js
@@ -0,0 +1,13 @@
+﻿
+var InstagramAPIGlobals = function () {
+
+    // this will be a private property
+    //Register a client with Instagram. Then replace that client_id in this string
+    var clientId = '311ae8765dc94a17a572c8624b180a1d';
+  
+    return {
+        // declare which properties and methods are supposed to be public
+        clientId:clientId
+
+    }
+}();
